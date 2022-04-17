8f808197cfcf3f48c867702387ba5d1346efbb7c
shubhamverma2000 <[vermashubham0608@gmail.com]>
2022-04-17 16:05:33 +0530

    Added templates folder in first_app and inherited main.html in various pages

--- first_app/views.py
+++ first_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'first_app/projects.html')
 
 def project(request):
-    return render(request, 'single_project.html')
+    return render(request, 'first_app/single_project.html')

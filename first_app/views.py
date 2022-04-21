885fd71b4e128ff342173d6af0649340d3e82e1f
shubhamverma2000 <[vermashubham0608@gmail.com]>
2022-04-21 19:16:04 +0530

    Rendered data in the templates

--- first_app/views.py
+++ first_app/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+databaselist=[
+    {
+        'name':'Shubham Verma',
+        'id' : '1',
+        'isStudent': True
+    },
+    {
+        'name':'Suyash Pathak',
+        'id' : '2',
+        'isStudent': False
+    }
+]
+
 # Create your views here.
-def projects(request):
-    return render(request, 'first_app/projects.html')
+def projects(request, pk):
+    projectObject = None
+    for i in databaselist:
+        if i['id']==str(pk):
+            projectObject= i
+    return render(request, 'first_app/projects.html', {'projectObject':projectObject }) 
 
 def project(request):
-    return render(request, 'first_app/single_project.html')
+    age=21
+    name='Shubham Verma'
+    context={'age':age, 'name':name, 'projects':databaselist}
+    return render(request, 'first_app/single_project.html', context)
